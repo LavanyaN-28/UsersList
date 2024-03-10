2b340934a62f5f817b719148ee745c297405d0a0
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-03-10 20:10:27 +0530

    ReactDOM,Portals and wrapper components

---
+++ App.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+
+function App() {
+  const [usersList,setUsersList] = useState([]);
+
+  const addUserHandler = (userName,userAge) => {
+    setUsersList((prevUserList) => {
+      return [...prevUserList,{name : userName , age : userAge, id: Math.random().toString()}];
+
+    })
+  }
+  return (
+    <>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </>
+  );
+}
+
+export default App;
